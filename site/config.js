b1126e2996d3d6b2d2eb62c5127fe8debfd203f8
Lucas Moreira <101739349+moreirahype@users.noreply.github.com>
2026-05-28 00:57:08 -0300

    Delete site directory

--- site/config.js
+++
@@ -1,5 +0,0 @@
-window.HOMESTUDIO_BI_CONFIG = {
-  apiUrl: 'https://script.google.com/macros/s/AKfycbxjAbvydrxB6I2et5e6FxuZuy6jzawlvcCnRXtDqPNYmzaSfKXQ5t5bWSoowrg4yVYnrg/exec',
-  defaultPeriod: 'today',
-  currency: 'BRL'
-};
